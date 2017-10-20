12243d3c4da7617a636a24d9b8f112023e85acda
Yuval Gabian <yuvalgab2007@gmail.com>
2017-10-20 15:34:29 +0300

    add send message code

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 (function() {
+  renderChat();
+
   setInterval(() => {
     renderChat();
   }, 2000);
-
-  renderChat();
 
   function renderChat() {
     const existMessages = document.querySelectorAll(".message");
@@ -29,6 +29,16 @@
     );
   }
 
+  function sendMessage(name, content) {
+    fetch("https://hidden-headland-7200.herokuapp.com/new", {
+      method: "POST",
+      body: JSON.stringify({ name, message: content }),
+      headers: { "Content-Type": "application/json" }
+    }).then(() => {
+      renderChat();
+    });
+  }
+
   function printMessage(name, content) {
     const messagesWindow = document.querySelector(".messages-window");
     let message = document.createElement("div");
@@ -41,4 +51,39 @@
     message.appendChild(contentPlace);
     messagesWindow.appendChild(message);
   }
+
+  const sendBtn = document.querySelector("button");
+
+  sendBtn.addEventListener("click", e => {
+    e.preventDefault();
+    const name = document.querySelector('[name="name"]').value;
+    const content = document.querySelector('[name="message"]').value;
+    const errorType = newMessageValidation(name, content);
+    if (errorType !== -1) {
+      return alert(errorsMessages[errorType]);
+    }
+
+    sendMessage(name, content);
+  });
+
+  function newMessageValidation(name, content) {
+    if (!name) {
+      return 0;
+    } else if (!content) {
+      return 1;
+    } else if (name.length > 20) {
+      return 2;
+    } else if (content.length > 100) {
+      return 3;
+    } else {
+      return -1;
+    }
+  }
+
+  const errorsMessages = [
+    "please insert name",
+    "please insert message",
+    "name can contain up to 10 characters",
+    "message can contain up to 50 characters"
+  ];
 })();
